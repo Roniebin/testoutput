e5cfe35aa6126cf9370f9453ddce606ce0d0d425
Roniebin <100742303+Roniebin@users.noreply.github.com>
2022-05-04 10:52:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+  include_package_daa = True,
+  name = 'testoutput1228',
+  version='0.0.1',
+  description = 'oss-dev my test1228',
+  author='Roni',
+  author_emal='killpig00@naver.com',
+  url = "https:://github.com/
+  install_requires=['pytest'],
+  long_description = 'text/markdown',
+  classifiers = [
+    "Programming Language :: python :: 3"
+    "operating System :: OS independent",
+  ],
+)
